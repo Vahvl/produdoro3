b32a35e0ccb95259379e0cbf82cf6d105c60b944
blueberious1 <118052173+blueberious1@users.noreply.github.com>
2025-04-04 13:23:34 +0300

    FIRST

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/teine')
+def teine():
+    link = '<p><a href="/">Koju tagasi</a></p>'
+    return("See on see teine" + link)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
